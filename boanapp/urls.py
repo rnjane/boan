0a923bc0840afe54e1d6ec79331630138f500ebd
Robert Njane <robert.njane@andela.com>
2019-07-31 15:20:52 +0300

    Add url mappings

--- boanapp/urls.py
+++ boanapp/urls.py
@@ -2,5 +2,7 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('get-data/', views.get_data, name='get_data'),
+    path('visualize-data/', views.visualize_data, name='visualize_data')
 ]
--- boanapp/views.py
+++ boanapp/views.py
@@ -3,3 +3,11 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'index.html')
+
+
+def visualize_data(request):
+    return render(request, 'index.html')
+
+
+def get_data(request):
+    return render(request, 'get-data.html')
