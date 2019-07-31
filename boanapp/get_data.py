85c42313d8debfb5921e1b30c35329cf34931108
Robert Njane <robert.njane@andela.com>
2019-07-31 19:05:15 +0300

    - Get data script - Model to store MAa

--- boanapp/get_data.py
+++ boanapp/get_data.py
@@ -1,16 +1,8 @@
-import time
-import datetime
-import os
+import time, datetime, os, json, ast, csv, pytz
 from iqoptionapi.stable_api import IQ_Option as api
-import json
-import ast
-import csv
-import pytz
 import pandas as pd
-
 from sqlalchemy import *
-
-from . import models
+from boanapp.pairs import assets
 
 
 def convert_date_time_to_epoch(inputdate):
@@ -18,10 +10,10 @@ def convert_date_time_to_epoch(inputdate):
     return timestamp
 
 
-def generate_times(finishtime, number_of_semi_days):
-    finish_time = convert_date_time_to_epoch(finishtime)
+def generate_times():
+    finish_time = 1564174799
     times_list = []
-    for i in range(number_of_semi_days):
+    for i in range(10):
         converted_time = datetime.datetime.fromtimestamp(
             finish_time, pytz.timezone("Africa/Nairobi")
         ).strftime("%Y-%m-%d %H:%M:%S")
@@ -32,7 +24,7 @@ def generate_times(finishtime, number_of_semi_days):
     return times_list
 
 
-def get_values(asset, finish_time, number_of_semi_days, candle_time):
+def get_values():
     iqemail = os.environ.get('iqemail')
     iqpasswd = os.environ.get('iqpasswd')
     try:
@@ -42,53 +34,47 @@ def get_values(asset, finish_time, number_of_semi_days, candle_time):
         print('Could not log in because of {0}'.format(e))
 
     epoch_times = generate_times(finish_time, number_of_semi_days)
-    for epoch_time in epoch_times:
-        cdle_list = []
-        my_candles = vals.get_candles(asset, candle_time, 500, epoch_time)
-        for candle in my_candles:
-            cdle = ast.literal_eval(json.dumps(candle))
-            del cdle['at']
-            del cdle['to']
-            del cdle['id']
-            del cdle['volume']
-            from_time = cdle['from']
-            from_time_converted = datetime.datetime.fromtimestamp(
-                from_time, pytz.timezone('Africa/Nairobi')).strftime('%Y-%m-%d %H:%M:%S')
-            del cdle['from']
-            cdle['timer'] = from_time_converted
-            cdle['pair'] = asset
-            x = cdle['close'] - cdle['open']
-            if x < 0:
-                cdle['greenred'] = -1
-            elif x == 0:
-                cdle['greenred'] = 0
-            else:
-                cdle['greenred'] = 1
-            cdle_list.append(cdle)
-        connection.execute(my_table.insert(), cdle_list)
-
-    # keys = cdle_list[0].keys()
-    # with open('nzdusd.csv', 'wb') as output_file:
-    #     dict_writer = csv.DictWriter(output_file, keys)
-    #     dict_writer.writeheader()
-    #     dict_writer.writerows(cdle_list)
 
     engine = create_engine(
-        'postgresql+psycopg2://robertnjane:roba1234@localhost/dataf')
+        os.environ.get('sqlalchemy_uri')
+    )
     connection = engine.connect()
-
     metadata = MetaData()
+    my_table = Table("boan_data", metadata, autoload_with=engine)
+    for instrument in assets:
+        print("getting values for {}".format(instrument))
+        times = generate_times()
+        for time in times:
+            cdle_list = []
+            my_candles = vals.get_candles(instrument, 60, 500, time)
+            for candle in my_candles:
+                cdle = ast.literal_eval(json.dumps(candle))
+                del cdle["at"]
+                del cdle["to"]
+                del cdle["id"]
+                del cdle["volume"]
+                from_time = cdle["from"]
+                from_time_converted = datetime.datetime.fromtimestamp(
+                    from_time, pytz.timezone("Africa/Nairobi")
+                ).strftime("%Y-%m-%d %H:%M:%S")
+                del cdle["from"]
+                cdle["timer"] = from_time_converted
+                cdle["pair"] = instrument
+                greenred = cdle["close"] - cdle["open"]
+                if greenred < 0:
+                    cdle["greenred"] = -1
+                elif greenred == 0:
+                    cdle["greenred"] = 0
+                else:
+                    cdle["greenred"] = 1
 
-    my_table = Table('dataapp_values', metadata, autoload_with=engine)
-
-    connection.execute(my_table.insert(), cdle_list)
-    keys = cdle_list[0].keys()
-    with open('data.csv', 'wb') as output_file:
-        dict_writer = csv.DictWriter(output_file, keys)
-        dict_writer.writeheader()
-        dict_writer.writerows(cdle_list)
-        import pdb
-        pdb.set_trace()
+                wkday = pd.Timestamp(from_time_converted)
+                if wkday.weekday() in range(0, 5):
+                    cdle_list.append(cdle)
+                else:
+                    pass
+            print("Saving values for {}".format(instrument))
+            connection.execute(my_table.insert(), cdle_list)
 
 
 get_values()
--- boanapp/models.py
+++ boanapp/models.py
@@ -9,3 +9,15 @@ class Values(models.Model):
     open = models.DecimalField(max_digits=15, decimal_places=8)
     close = models.DecimalField(max_digits=15, decimal_places=8)
     pair = models.CharField(max_length=10)
+
+
+class ValuesMA(models.Model):
+    min = models.DecimalField(max_digits=15, decimal_places=8)
+    max = models.DecimalField(max_digits=15, decimal_places=8)
+    greenred = models.IntegerField()
+    timer = models.DateTimeField()
+    open = models.DecimalField(max_digits=15, decimal_places=8)
+    close = models.DecimalField(max_digits=15, decimal_places=8)
+    pair = models.CharField(max_length=10)
+    ma14 = models.DecimalField(max_digits=15, decimal_places=8)
+    money = models.IntegerField()
