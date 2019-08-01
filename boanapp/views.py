474b9d4ef7b3862122835bd82cf793a87b488f61
Robert Njane <robert.njane@andela.com>
2019-08-01 19:15:49 +0300

    get candle data

--- boanapp/views.py
+++ boanapp/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render, redirect
 from boanapp.pairs import assets
-from boanapp import labouchere
+from boanapp import labouchere, logic
 
 
 def visualize_data(request):
     context = {}
+    my_list = logic.get_moneies_list('EURUSD')
 
     context['pairs'] = assets
     context['hours'] = [hour for hour in range(24)]
