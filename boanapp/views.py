7587ca0ab66ff7bd6da70fdafec817ef62d2f043
Robert Njane <robert.njane@andela.com>
2019-08-08 16:43:21 +0300

    tabulate data

--- boanapp/views.py
+++ boanapp/views.py
@@ -36,7 +36,10 @@ def visulize_defined_data(request):
         'asset'), split_date[2], split_date[0], split_date[1])
     for hour in hours:
         profit = labouchere.ProphetC().get_profit(hours[hour])
-        hourly_profit.append(profit)
+        if profit < 0:
+            hourly_profit.append(0)
+        else:
+            hourly_profit.append(1)
     context['pairs'] = assets
     context['profits'] = hourly_profit
     context['hours'] = [hour for hour in range(24)]
@@ -47,21 +50,36 @@ def visulize_defined_data(request):
     return render(request, 'index.html', context)
 
 
-@cache_page(CACHE_TTL)
+# @cache_page(CACHE_TTL)
 def tabular_data(request):
     context = {}
-    # hourly_profit = []
-    hourly_profit_dict = {}
-    top_assets = pairs.assets[:10]
+    top_assets = pairs.assets[:2]
     all_dates = pd.date_range(start=logic.get_last_date(),
                               end=logic.get_start_date()).strftime("%m/%d/%Y").to_list()
-    for date in all_dates:
-        for asset in top_assets:
+    hours_dict = {}
+
+    for asset in top_assets:
+        hourly_profit_dict = {}
+        for date in all_dates:
             hours = logic.get_moneies_list(
                 asset, date.split('/')[2], date.split('/')[0], date.split('/')[1])
             for hour in hours:
                 profit = labouchere.ProphetC().get_profit(hours[hour])
-                hourly_profit_dict['date'] = [asset, profit]
-    context['assets'] = top_assets
-    context['hours'] = [hour for hour in range(24)]
+                if profit < 0:
+                    try:
+                        hourly_profit_dict[hour]
+                        hourly_profit_dict[hour] += 1
+                    except:
+                        hourly_profit_dict[hour] = 1
+                else:
+                    try:
+                        hourly_profit_dict[hour]
+                        hourly_profit_dict[hour] += 0
+                    except:
+                        hourly_profit_dict[hour] = 0
+        hours_dict[asset] = hourly_profit_dict
+
+    # context['data'] = hours_dict
+    context['assets'] = list(hours_dict.keys())
+    context['values'] = [i for i in hours_dict.values()]
     return render(request, 'tabular-data.html', context)
