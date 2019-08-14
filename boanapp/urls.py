0ad274e0d6fe7a6202ba3c0184a752e82966da07
Robert Njane <robert.njane@andela.com>
2019-08-14 16:59:12 +0300

    Choose asset

--- boanapp/urls.py
+++ boanapp/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('visualize-data/', views.visualize_data, name='visualize_data'),
     path('visualize-specified-data/',
          views.visulize_defined_data, name='visulize_defined_data'),
-    path('tabular-data', views.tabular_data, name='tabular_data')
+    path('tabular-data/', views.tabular_data, name='tabular_data')
 ]
--- boanapp/views.py
+++ boanapp/views.py
@@ -1,3 +1,4 @@
+import json
 from django.shortcuts import render, redirect
 from django.conf import settings
 from django.views.decorators.cache import cache_page
@@ -50,36 +51,83 @@ def visulize_defined_data(request):
     return render(request, 'index.html', context)
 
 
+# def choose_asset(request):
+#     context = {}
+
+
 # @cache_page(CACHE_TTL)
 def tabular_data(request):
     context = {}
     top_assets = pairs.assets[:2]
     all_dates = pd.date_range(start=logic.get_last_date(),
                               end=logic.get_start_date()).strftime("%m/%d/%Y").to_list()
-    hours_dict = {}
 
+    total_profit_dict = {}
     for asset in top_assets:
-        hourly_profit_dict = {}
-        for date in all_dates:
-            hours = logic.get_moneies_list(
-                asset, date.split('/')[2], date.split('/')[0], date.split('/')[1])
-            for hour in hours:
-                profit = labouchere.ProphetC().get_profit(hours[hour])
-                if profit < 0:
-                    try:
-                        hourly_profit_dict[hour]
-                        hourly_profit_dict[hour] += 1
-                    except:
-                        hourly_profit_dict[hour] = 1
-                else:
-                    try:
-                        hourly_profit_dict[hour]
-                        hourly_profit_dict[hour] += 0
-                    except:
-                        hourly_profit_dict[hour] = 0
-        hours_dict[asset] = hourly_profit_dict
+        daily_profit_dict = {}
+        # for date in all_dates:
+        #     hours = logic.get_moneies_list(
+        #         asset, date.split('/')[2], date.split('/')[0], date.split('/')[1])
+        #     dips = 0
+        #     for hour in hours:
+        #         profit = labouchere.ProphetC().get_profit(hours[hour])
+        #         if profit < 0:
+        #             dips += 1
+        #         daily_profit_dict[date] = dips
 
-    # context['data'] = hours_dict
-    context['assets'] = list(hours_dict.keys())
-    context['values'] = [i for i in hours_dict.values()]
+        total_profit_dict[asset] = daily_profit_dict
+    x = {'AUDUSD': {'07/22/2019': 3, '07/23/2019': 8, '07/24/2019': 7, '07/25/2019': 7, '07/26/2019': 3},
+         'AUDCAD': {'07/22/2019': 6, '07/23/2019': 8, '07/24/2019': 6, '07/25/2019': 7, '07/26/2019': 2}}
+    context['data'] = x
+    context['labels'] = json.dumps([i for i in x])
+    context['vals'] = json.dumps(
+        [{'AUDUSD': [3, 8, 7, 7, 3]}, {'AUDCAD': [6, 8, 6, 7, 2]}])
+    context['dates2'] = json.dumps(["07/22/2019", "07/23/2019",
+                                    "07/24/2019", "07/25/2019", "07/26/2019"])
+
+    # context['datasets'] = r"[{
+    #     label: '# of Votes',
+    #     data: [12, 19, 3, 5, 2],
+    #     backgroundColor: [
+    #         'rgba(255, 99, 132, 0.2)',
+    #         'rgba(54, 162, 235, 0.2)',
+    #         'rgba(255, 206, 86, 0.2)',
+    #         'rgba(75, 192, 192, 0.2)',
+    #         'rgba(153, 102, 255, 0.2)',
+    #         'rgba(255, 159, 64, 0.2)'
+    #     ],
+    #     borderColor: [
+    #         'rgba(255, 99, 132, 1)',
+    #         'rgba(54, 162, 235, 1)',
+    #         'rgba(255, 206, 86, 1)',
+    #         'rgba(75, 192, 192, 1)',
+    #         'rgba(153, 102, 255, 1)',
+    #         'rgba(255, 159, 64, 1)'
+    #     ],
+    #     borderWidth: 1
+    # },
+    #     {
+    #     label: '# of Votes',
+    #     data: [22, 29, 32, 25, 22],
+    #     backgroundColor: [
+    #         'rgba(255, 99, 132, 0.2)',
+    #         'rgba(54, 162, 235, 0.2)',
+    #         'rgba(255, 206, 86, 0.2)',
+    #         'rgba(75, 192, 192, 0.2)',
+    #         'rgba(153, 102, 255, 0.2)',
+    #         'rgba(255, 159, 64, 0.2)'
+    #     ],
+    #     borderColor: [
+    #         'rgba(255, 99, 132, 1)',
+    #         'rgba(54, 162, 235, 1)',
+    #         'rgba(255, 206, 86, 1)',
+    #         'rgba(75, 192, 192, 1)',
+    #         'rgba(153, 102, 255, 1)',
+    #         'rgba(255, 159, 64, 1)'
+    #     ],
+    #     borderWidth: 1
+    # }]"
+    # context['dates'] = [list(i.keys())
+    #                     for i in list(total_profit_dict.values())][0]
+    # context['values'] = [i for i in hours_dict.values()]
     return render(request, 'tabular-data.html', context)
